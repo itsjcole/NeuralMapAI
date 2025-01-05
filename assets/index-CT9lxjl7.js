e3acdcaa974cb379208a69df742a25c2dab728b1
itsjcole <itsandrewtate1@gmail.com>
2025-01-05 20:10:28 +0800

    Update index-CT9lxjl7.js

--- assets/index-CT9lxjl7.js
+++ assets/index-CT9lxjl7.js
@@ -718,11 +718,19 @@ const ye = j("X", [["path", {
                                     children: "Whitepaper"
                                 })]
                             }), e.jsx("a", {//Changeafter
-                                href: "https://www.google.com",
+                                href: "https://x.com/",
                                 target: "_blank",
                                 rel: "noopener noreferrer",
                                 className: `${s.primary} hover:opacity-80 transition-opacity`,
                                 children: e.jsx(ye, {
+                                    className: "w-5 h-5"
+                                })
+                            }), , e.jsx("a", {//Changeafte
+                                href: "https://t.me/",
+                                target: "_blank",
+                                rel: "noopener noreferrer",
+                                className: `${s.primary} hover:opacity-80 transition-opacity`,
+                                children: e.jsx(fe, {
                                     className: "w-5 h-5"
                                 })
                             })]
